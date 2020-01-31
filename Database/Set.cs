d0ca7ebdf3074e3348263f5c3420c1423f14a06e
Elpulgo <elpulgo@users.noreply.github.com>
2020-01-31 12:20:29 +0100

    Upload files

---
+++ Database/Set.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tester.Database
+{
+    public class Set
+    {
+        public int Id { get; set; }
+
+        public int RoundId { get; set; }
+
+        public int PlayerId { get; set; }
+
+        public int Score { get; set; }
+
+        public int ScoreOffset { get; set; }
+    }
+}
