6c73ffe8f023c08539e3a335a32fea66c411e8c8
Oscar Larsson <oscar.larsson@positiongreen.com>
2024-04-13 16:52:17 +0200

    Updated to net7

---
+++ net8/parser/Database/Hole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tester.Database
+{
+    public class Hole
+    {
+        public int Id { get; set; }
+
+        public int CourseId { get; set; }
+
+        public int Par { get; set; }
+
+        public int Number { get; set; }
+    }
+}
---
+++ net8/parser/Database/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tester.Database
+{
+    public class Player
+    {
+        public Player()
+        {
+            Created = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Alias { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string PhotoFilePath { get; set; }
+
+        public bool IsMain { get; set; }
+    }
+}
